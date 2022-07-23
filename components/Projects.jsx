1066c349084812f3d363a90d3d0816759fbef463
DevMattDavies <mdavies.me@pm.me>
2022-07-23 12:57:30 +0100

    Project slider mapped, final content to add

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,20 +1,77 @@
+import styled from "styled-components";
+import Image from "next/future/image";
+import Slides from "../data/slides/SlidesData";
 import { SectionContainer, SectionContentContainer } from "./Containers.styles";
 import { SectionTitle } from "../components/SectionTitle.styles";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css/core";
+import "@splidejs/react-splide/css/skyblue";
+
+
+const SlideContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: max-content;
+  gap: 2rem;
+`;
+
+const LeftSlideContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  gap: 2rem;
+`;
+const RightSlideContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const SlideIconContainer = styled.div`
+  display: flex;
+  position: relative;
+  gap: 1rem;
+`;
+
+const ProjectImageStyle = {
+  height: "auto",
+  width: "100%",
+};
+
+const ProjectTitle = styled.h3`
+  color: var(--secondary);
+  font-size: 2rem;
+  font-weight: 300;
+`;
+
 
 function Projects() {
   return (
     <SectionContainer>
       <SectionTitle>Projects</SectionTitle>
       <SectionContentContainer>
-        <Splide aria-label="My Favorite Images">
-          <SplideSlide>
-            <img src="image1.jpg" alt="Image 1" />
-          </SplideSlide>
-          <SplideSlide>
-            <img src="image2.jpg" alt="Image 2" />
-          </SplideSlide>
+        <Splide aria-label="My Projects">
+          {Slides.map((slide) => {
+            return (
+              <SplideSlide>
+                <SlideContainer>
+                  <LeftSlideContainer>
+                    <Image src={slide.src} style={ProjectImageStyle}></Image>
+                    <SlideIconContainer>
+                      {Object.values(slide.icons).map((icon) => {
+                        return (
+                          <Image src={icon} width={50} height={50}></Image>
+                        );
+                      })}
+                    </SlideIconContainer>
+                  </LeftSlideContainer>
+                  <RightSlideContainer>
+                    <ProjectTitle>{slide.title}</ProjectTitle>
+                  </RightSlideContainer>
+                </SlideContainer>
+              </SplideSlide>
+            );
+          })}
         </Splide>
       </SectionContentContainer>
     </SectionContainer>
---
+++ data/slides/SlidesData.js
@@ -0,0 +1,26 @@
+import WordleImg from "../../assets/project_img/wordle.png"
+import SamuraiImg from "../../assets/project_img/snippet_samurai.png"
+import Icon from "../../public/css.png"
+
+const Slides = [
+  {
+    title: "Wordle in React",
+    text: "text here",
+    src: WordleImg,
+    icons: {
+      Icon1: Icon,
+      Icon2: Icon,
+    },
+  },
+  {
+    title: "Snippet Samurai",
+    text: "text here",
+    src: SamuraiImg,
+    icons: {
+      Icon1: Icon,
+      Icon2: Icon,
+    },
+  },
+];
+
+export default Slides;
